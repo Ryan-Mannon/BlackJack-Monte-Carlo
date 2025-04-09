45e7ac54432f835a2ae793af4702602c76925c71
Ryan Mannon <ryanmannon2002@gmail.com>
2025-04-08 21:10:17 -0700

    commit

--- blackjack.cpp
+++ blackjack.cpp
@@ -6,30 +6,49 @@
 
 using namespace std;
 
+
 //deck with each face card counting as a 10
 //TODO: Add in aces for either a 1 or 11
 const int deckSize = 52;
+int hand = 0;
 
-int deck[deckSize] = {2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10}; 
+int deck[deckSize] = {1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10}; 
 
-//simple function to draw two cards assuming infinite deck
-int drawHand(){
-    int hand = 0;
-    hand += deck[rand()%52];
-    hand += deck[rand()%52];
-return hand;
+//simple function to draw a card from deck
+int hit() {
+    // Generate a random index for array
+    int index = rand() % deckSize;
+    if (deck[index] == 1 && hand <=10){
+        cout << "added 11" << endl;    //for testing
+        hand += 11;
+        return deck[index];
+    } else if(deck[index]==1) {
+        cout << "added 1" << endl;    //for testing
+        hand += 1;
+        return deck[index];
+    }
+    cout << "added " << deck[index] << endl;    //for testing
+    hand += deck[index];
+    return deck[index];
 }
 
+int startingHand(){
+    hit();
+    hit();
+    return hand;
+}
 
 int main(){
-
-    int seed = 12345;
     srand(time(NULL));      //random time seed
 
-    for(int i = 0; i < 10; i++){
-        int h = drawHand();
+    startingHand();
+    cout << "hand:" << hand;
+    
+
+    /*for(int i = 0; i < 10; i++){
+        int h = hit();
         cout << h << endl;
-    }
+    }*/
 
 return 0;
 }
