7155cb60c3249a6023607bb92baddffae9842243
Ryan Mannon <ryanmannon2002@gmail.com>
2025-04-08 19:47:20 -0700

    Wrote basic function to draw two cards from an infinite deck with no aces and add them together to make a hand.

--- blackjack.cpp
+++ blackjack.cpp
@@ -8,14 +8,28 @@ using namespace std;
 
 //deck with each face card counting as a 10
 //TODO: Add in aces for either a 1 or 11
-int deck[52] = {2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10}; 
+const int deckSize = 52;
 
+int deck[deckSize] = {2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10}; 
+
+//simple function to draw two cards assuming infinite deck
 int drawHand(){
-return 0;
+    int hand = 0;
+    hand += deck[rand()%52];
+    hand += deck[rand()%52];
+return hand;
 }
 
 
 int main(){
-//test
-    return 0;
+
+    int seed = 12345;
+    srand(time(NULL));      //random time seed
+
+    for(int i = 0; i < 10; i++){
+        int h = drawHand();
+        cout << h << endl;
+    }
+
+return 0;
 }
