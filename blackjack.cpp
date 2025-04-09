905eca0d9d7974d33366016ef09c7f35ab721f99
Ryan-Mannon <102073973+Ryan-Mannon@users.noreply.github.com>
2025-04-08 22:25:33 -0700

    new

--- blackjack.cpp
+++ blackjack.cpp
@@ -8,47 +8,118 @@ using namespace std;
 
 
 //deck with each face card counting as a 10
-//TODO: Add in aces for either a 1 or 11
+//TODO: 
+//Add in aces for either a 1 or 11
+//so far I have to where each ace counts as an 11. If the hand is greater than 21 then subtract 10 for each ace until hand is less than 21
 const int deckSize = 52;
-int hand = 0;
+int pHand = 0;
+int aceCounter = 0;
+int dHand = 0;
+int gameResult;     //0 for loss 1 for tie 2 for win
+int score;
 
-int deck[deckSize] = {1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10}; 
+int deck[deckSize] = {11,11,11,11,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10}; 
+
+void aceCheck (){
+    if(pHand > 21 && aceCounter > 0){
+        pHand -= 10;
+        aceCounter--;
+    }
+}
+
+int result(){
+    if (pHand > 21)
+        return 0;
+    if (pHand == dHand)
+        return 1;
+    if(dHand > 21)
+        return 2;
+    if(pHand > dHand)
+        return 2;
+    return -1;
+}
 
 //simple function to draw a card from deck
 int hit() {
     // Generate a random index for array
     int index = rand() % deckSize;
-    if (deck[index] == 1 && hand <=10){
-        cout << "added 11" << endl;    //for testing
-        hand += 11;
-        return deck[index];
-    } else if(deck[index]==1) {
-        cout << "added 1" << endl;    //for testing
-        hand += 1;
-        return deck[index];
+    pHand += deck[index];
+    if (deck[index] == 11 ){
+        aceCounter++;
     }
-    cout << "added " << deck[index] << endl;    //for testing
-    hand += deck[index];
+    //cout << "added " << deck[index] << endl;    //for testing
     return deck[index];
 }
+
+int dHit() {
+    // Generate a random index for array
+    int index = rand() % deckSize;
+    dHand += deck[index];
+    if (deck[index] == 11 ){
+        aceCounter++;
+    }
+    return deck[index];
+}
+
 
 int startingHand(){
     hit();
     hit();
-    return hand;
+    return pHand;
+}
+
+
+//Dealer will hit until they get 17 or above
+int dealerHand(){
+    while (dHand < 17){
+        dHit();
+    }
+    return dHand;
+}
+
+int runPolicy(int policy, int gamesPlayed){
+    gameResult = result();
+
+    switch (gameResult)
+    {
+    case 0:
+        score--;
+        break;
+    case 1:
+        break;
+    case 2:
+        score++;
+        break;
+    default:
+        break;
+    }
+    
+    switch (policy)
+    {
+    case 1:
+        for(int i = 0; i < gamesPlayed; i++){
+            while(pHand < 17){
+                startingHand();
+                dealerHand();
+                hit();
+            }
+        }
+        break;
+    
+    default:
+        break;
+    }
+    double winRate = score/gamesPlayed;
+    cout << score;
+    return winRate;
 }
 
 int main(){
     srand(time(NULL));      //random time seed
 
-    startingHand();
-    cout << "hand:" << hand;
+    // runPolicy(policy,gamesPlayed)
+    cout << runPolicy(1,10);
     
-
-    /*for(int i = 0; i < 10; i++){
-        int h = hit();
-        cout << h << endl;
-    }*/
 
 return 0;
 }
